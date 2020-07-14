6ddbd5a052881635277b792ca77ab4e0fa2d89f9
Michael Spohn <mikeLspohn@gmail.com>
2020-07-14 18:54:31 -0500

    Initial Commit

---
+++ BikeShops/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BikeShops
+//
+//  Created by Michael Spohn on 7/14/20.
+//  Copyright © 2020 Learning Mobile Apps. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
